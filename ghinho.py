b0e4217638ab94201370c0910cea89581179c61f
phamcongdan85 <congdan.pham@gmail.com>
2022-08-07 09:19:06 +0700

    Update and rename hi.py to ghinho.py

---
+++ ghinho.py
@@ -0,0 +1,3 @@
+1. 
+Để cài các gói cần thiết trong thư mục đã tải về: 
+pip install -r requirements.txt --no-index --find-links download
--- hi.py
+++
@@ -1,3 +0,0 @@
-import numpy as np
-
-A = np.array([[1, 2], [4,6]])
