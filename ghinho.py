3662998ca9ff003f618342f2e3ec99a91ac2e894
phamcongdan85 <congdan.pham@gmail.com>
2022-08-07 09:20:42 +0700

    Update ghinho.py

--- ghinho.py
+++ ghinho.py
@@ -1,3 +1,8 @@
 1. 
 Để cài các gói cần thiết trong thư mục đã tải về: 
 pip install -r requirements.txt --no-index --find-links download
+
+
+Dong bo cac goi cai dat giua cac may tinh
+pip freeze > requirements.txt
+pip install --download=/path/to/packages/downloaded -r requirements.txt
